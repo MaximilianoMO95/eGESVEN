2bcf3a4f3e45e779874e9f0f484ab6ebe9e0addd
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-28 21:41:35 -0400

    feat(backend): add complete FastAPI and MySQL project template

---
+++ backend/app/core/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise EnvironmentError(f"Required environment variable -> 'DATABASE_URL' is not set.")
+
+
+# Note: For SQLITE you have to aditionally pass "connect_args={"check_same_thread": False}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
