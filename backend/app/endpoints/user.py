30fb6fc4db58005768495d6c802dcbe790bf9048
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-29 16:56:25 -0400

    refactor(backend): update user crud and endpoint

--- backend/app/endpoints/user.py
+++ backend/app/endpoints/user.py
@@ -23,9 +23,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud_user.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
+
     return db_user
 
 
 @router.get("/users/", response_model=list[User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud_user.get_users(db=db, skip=skip, limit=limit)
