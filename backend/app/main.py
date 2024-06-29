7cef62327a26860b75f80574c2496bb02275c798
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-29 16:56:55 -0400

    feat(backend): add product api stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
 from app.core.db import engine
-from app.endpoints import user
+from app.endpoints import user, product
 from app import models
 
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/api/v1", tags=["user"])
+app.include_router(product.router, prefix="/api/v1", tags=["product"])
 
 
 # Create database tables
