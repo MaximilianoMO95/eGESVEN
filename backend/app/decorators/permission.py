ae65f6be5c548c45a655a733948387d86ccbbda8
mr-ema <ema.a.v.c2017@gmail.com>
2024-07-04 22:20:11 -0400

    feat(backend): add endpoints permission decorator

---
+++ backend/app/decorators/permission.py
@@ -0,0 +1,70 @@
+from functools import wraps
+from typing import Any, cast
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from app.endpoints.deps import get_db
+from app.models.user import Permission, Role, User
+
+
+def required_permission(permission_name: str, description: str = ""):
+    """
+    A decorator that registers a permission in the database and checks if the current user has the required permission.
+
+    Args:
+        permission_name (str): The name of the permission required for the route.
+        description (str): A brief description of the permission (optional).
+
+    Returns:
+        The decorated route function.
+    """
+    def decorator(func):
+        @wraps(func)
+        async def wrapper( *args: Any, **kwargs: Any) -> Any:
+            # Check if the user has the required permission
+            user_permissions = get_current_user_permissions()
+            if permission_name not in user_permissions:
+                raise HTTPException(status_code=403, detail="You do not have the required permissions")
+
+            return await func(*args, **kwargs)
+
+
+        _ = register_permission_in_db(permission_name, description)
+
+        # Add the required permission to the route
+        wrapper.__permission__ = permission_name
+        return wrapper
+
+    return decorator
+
+
+def register_permission_in_db(permission_name: str, description: str) -> None:
+    db: Session = next(get_db())
+
+    permission = db.query(Permission).filter(Permission.name == permission_name).first()
+    if not permission:
+        permission = Permission(name=permission_name, description=description)
+        db.add(permission)
+        db.commit()
+        db.refresh(permission)
+
+
+def get_current_user(db: Session) -> User:
+    # TODO: implement logic
+    user = db.query(User).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not authenticated")
+
+    return user
+
+
+def get_current_user_permissions() -> list[str]:
+    db: Session = next(get_db())
+    current_user = get_current_user(db)
+
+    user_role = db.query(Role).filter(Role.id == current_user.role_id).first()
+    if not user_role:
+        raise HTTPException(status_code=403, detail="No role assigned")
+
+    permissions = db.query(Permission).join(Role.permissions).filter(Role.id == user_role.id).all()
+    return [cast(str, perm.name) for perm in permissions]
