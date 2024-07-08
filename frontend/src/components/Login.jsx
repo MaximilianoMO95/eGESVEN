a3f043cfb2e6efddb1edbd4421a78ef7e69b2461
MaximilianoMO95 <131606838+MaximilianoMO95@users.noreply.github.com>
2024-07-08 17:11:15 -0400

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,6 +11,16 @@ function Login() {
   const { authState, login } = useAuth();
   const navigate = useNavigate();
 
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   useEffect(() => {
     if (authState.isAuthenticated) {
       navigate('/Productos');
@@ -48,7 +58,7 @@ function Login() {
   };
 
   return (
-    <div className={`${LoginCSS.container}`}>
+    <div className={`${LoginCSS.container} ${isActive ? LoginCSS.active : ''}`}>
       <div className={`${LoginCSS.formBox} ${LoginCSS.login}`}>
         <h2>Login</h2>
         <form onSubmit={handleLoginSubmit}>
@@ -76,10 +86,36 @@ function Login() {
           </div>
           {errorMessage && <p className={LoginCSS.error}>{errorMessage}</p>}
           {successMessage && <p className={LoginCSS.success}>{successMessage}</p>}
+          <div className={LoginCSS.noAccount}>
+            <p>No tienes cuenta? <a href="#" className={LoginCSS.registerLink} onClick={handleRegisterClick}>Registrate</a></p>
+          </div>
           <button className={LoginCSS.btnLogin} type="submit">Login</button>
         </form>
       </div>
+
+      <div className={`${LoginCSS.formBox} ${LoginCSS.register}`}>
+        <h2>Registro</h2>
+        <form action="">
+          <div className={LoginCSS.inputBox}>
+            <input type="text" name="username" id="username" placeholder="Username" />
+            <i className="bx bxs-user"></i>
+          </div>
+          <div className={LoginCSS.inputBox}>
+            <input type="email" name="email" id="email" placeholder="Email" />
+            <i className="bx bxs-envelope"></i>
+          </div>
+          <div className={LoginCSS.inputBox}>
+            <input type="password" name="password" id="password" placeholder="Password"/>
+            <i className="bx bxs-lock-alt"></i>
+          </div>
+          <div className={LoginCSS.noAccount}>
+            <p>Ya tienes cuenta? <a href="#" className={LoginCSS.loginLink} onClick={handleLoginClick}>Login</a></p>
+          </div>
+          <button className={LoginCSS.btnLogin} type="submit">Registro</button>
+        </form>
+      </div>
     </div>
+    
   );
 }
 
